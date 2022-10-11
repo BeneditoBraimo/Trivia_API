c7c5f62bbb6790f91129988d62772d0db2661e11
BeneditoBraimo <beneditobraimo@gmail.com>
2022-10-11 13:21:19 +0200

    added test_get_category() method

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
+        self.database_user = "postgres"
+        self.database_password = "abc"
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(self.database_user, self.database_password,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +35,12 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
-
+    def test_get_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertGreater(len(data["categories"]), 0)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
